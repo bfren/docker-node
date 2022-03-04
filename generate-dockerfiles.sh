a305abd4356098fade1127e7feafaa162cec529a
Ben Green <ben@bcgdesign.com>
2022-03-04 07:49:58 +0000

    Generate Dockerfiles

---
+++ generate-dockerfiles.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -euo pipefail
+
+docker pull bfren/alpine
+
+BASE_REVISION="4.0.10"
+echo "Base: ${BASE_REVISION}"
+
+NODE_VERSIONS="16"
+for V in ${NODE_VERSIONS} ; do
+
+    echo "NodeJS ${V}"
+    ALPINE_MINOR=`cat ./${V}/ALPINE_MINOR`
+
+    DOCKERFILE=$(docker run \
+        -v ${PWD}:/ws \
+        -e BF_DEBUG=0 \
+        bfren/alpine esh \
+        "/ws/Dockerfile.esh" \
+        BASE_REVISION=${BASE_REVISION} \
+        ALPINE_MINOR=${ALPINE_MINOR} \
+        NODE_MAJOR=${V}
+    )
+
+    echo "${DOCKERFILE}" > ./${V}/Dockerfile
+
+done
+
+docker system prune -f
+echo "Done."
---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=`cat VERSION`
+
+git pull || exit 1
+chmod +x run.sh
+
+docker buildx build \
+    --build-arg BF_IMAGE=node \
+    --build-arg BF_VERSION=${VERSION} \
+    -f 16/Dockerfile \
+    -t node-dev \
+    . \
+    && \
+    docker run -it node-dev sh
