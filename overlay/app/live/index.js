c7efa57fca5d6eb6e932231bf3497ce9abe335d8
bfren <bf@bfren.dev>
2022-09-22 08:16:34 +0100

    Using environment variable to set the Node port

--- overlay/app/live/index.js
+++ overlay/app/live/index.js
@@ -1,12 +1,12 @@
 // Import http module
-const http = require( 'http' );
+import { createServer } from 'http';
 
 // Hosting values
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.NODE_PORT;
 
 // Create server
-const server = http.createServer((req, res) => {
+const server = createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.write('<!DOCTYPE html><html>');
