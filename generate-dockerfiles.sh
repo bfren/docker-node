0b7d6a0dbfe22b9f6537d8c5b4c54434048a1ad2
bfren <bf@bfren.dev>
2026-06-28 09:31:01 +0100

    Using latest base images

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 docker pull bfren/alpine
 
-BASE_VERSION="6.1.9"
+BASE_VERSION="6.2.0"
 echo "Base: ${BASE_VERSION}"
 
-NODE_VERSIONS="16 17 18 19 20 21 22 23 24"
+NODE_VERSIONS="16 17 18 19 20 21 22 23 24 26"
 for V in ${NODE_VERSIONS} ; do
 
     echo "Node.js ${V}"
